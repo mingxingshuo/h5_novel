29b7c65b501574b3c675681919c3f34cd2e74fcb
DESKTOP-1C3BJVH\明星说-李鑫 <weblixin@163.com>
2019-02-22 10:47:26 +0800

    ok

--- routes/chapters.js
+++ routes/chapters.js
@@ -6,50 +6,83 @@ router.prefix('/chapter')
 var price = 30
 
 router.get('/all', async function (ctx, next) {
-    let bid = ctx.request.query.bid;
-    let chapter = await ChapterModel.find({bid: bid})
-    ctx.body = {success: '成功', data: chapter}
+  let bid = ctx.request.query.bid;
+  let chapter = await ChapterModel.find({
+    bid: bid
+  })
+  ctx.body = {
+    success: '成功',
+    data: chapter
+  }
 })
 
 router.get('/', async function (ctx, next) {
-    let id = ctx.request.query.id
-    let unionid = ctx.request.query.unionid
-    let chapter = await ChapterModel.findOne({id: id})
-    let user = await UserModel.findOne({unionid: unionid})
-    let mem_user = await mem.get("novelUser_" + unionid)
-    console.log(id, unionid, chapter, user, '---------------------chapter')
-    if (!mem_user) {
-        return ctx.body = {err: "请先登录"}
+  let id = ctx.request.query.id
+  let unionid = ctx.request.query.unionid
+  let chapter = await ChapterModel.findOne({
+    id: id
+  })
+  let user = await UserModel.findOne({
+    unionid: unionid
+  })
+  // let mem_user = await mem.get("novelUser_" + unionid)
+  // console.log(id, unionid, chapter, user, '---------------------chapter')
+  // if (!mem_user) {
+  //     return ctx.body = {err: "请先登录"}
+  // }
+  if (!chapter.isvip) {
+    console.log(0)
+    return ctx.body = {
+      success: '成功',
+      data: chapter
     }
-    if (!chapter.isvip) {
-        return ctx.body = {success: '成功', data: chapter}
+  }
+  if (user.isvip) {
+    console.log(1)
+    return ctx.body = {
+      success: '成功',
+      data: chapter
     }
-    if (user.isvip) {
-        return ctx.body = {success: '成功', data: chapter}
+  }
+  let pay_chapter = user.pay_chapter.indexOf(id)
+  if (pay_chapter != -1) {
+    console.log(2)
+    return ctx.body = {
+      success: '成功',
+      data: chapter
     }
-    let pay_chapter = user.pay_chapter.indexOf(id)
-    if (pay_chapter != -1) {
-        return ctx.body = {success: '成功', data: chapter}
-    } else {
-        if (user.balance > price) {
-            user.update({
-                $addToSet: {pay_chapter: id},
-                $inc: {balance: -price}
-            })
-            return ctx.body = {success: '成功', data: chapter}
-        } else {
-            return ctx.body = {err: "您的余额不足，请及时充值"}
+  } else {
+    if (user.balance > price) {
+      UserModel.findOneAndUpdate({unionid: unionid},{
+        $addToSet: {
+          pay_chapter: id
+        },
+        $inc: {
+          balance: -price
         }
+      })
+      console.log(3)
+      return ctx.body = {
+        success: '成功',
+        data: chapter
+      }
+    } else {
+      console.log(4)
+      return ctx.body = {
+        err: "您的余额不足，请及时充值"
+      }
     }
+  }
 })
 
-router.get('/reset', async(ctx, next) => {
-    var chapter = new ChapterModel()
-    chapter.nextCount(function (err, count) {
-        chapter.resetCount(function (err, nextCount) {
-        });
-    });
-    ctx.body = {success: '重置成功'}
+router.get('/reset', async (ctx, next) => {
+  var chapter = new ChapterModel()
+  chapter.nextCount(function (err, count) {
+    chapter.resetCount(function (err, nextCount) {});
+  });
+  ctx.body = {
+    success: '重置成功'
+  }
 })
 
 
--- routes/users.js
+++ routes/users.js
@@ -6,79 +6,127 @@ var mem = require('../util/mem.js');
 router.prefix('/user')
 
 router.get('/', async function (ctx, next) {
-    let unionid = ctx.request.query.unionid
-    let mem_user = await mem.get("novelUser_" + unionid)
-    if (mem_user) {
-        let user = await UserModel.findOne({unionid: unionid})
-        ctx.body = {success: '成功', data: user}
-    } else {
-        ctx.body = {err: "您还没有登陆，请先登录"}
-    }
+  let user = await UserModel.find()
+  ctx.body = {
+    success: '成功',
+    data: user
+  }
 })
 
+// router.get('/', async function (ctx, next) {
+//   let unionid = ctx.request.query.unionid
+//   let mem_user = await mem.get("novelUser_" + unionid)
+//   if (mem_user) {
+//       let user = await UserModel.findOne({unionid: unionid})
+//       ctx.body = {success: '成功', data: user}
+//   } else {
+//       ctx.body = {err: "您还没有登陆，请先登录"}
+//   }
+// })
+
+
 router.get('/login', async function (ctx, next) {
-    let unionid = ctx.request.query.unionid
-    let openid = ctx.request.query.openid
-    let code = ctx.request.query.code
-    let user = await UserModel.findOne({unionid: unionid})
-    if(unionid) {
-        if (user) {
-            await mem.set("novelUser_" + unionid, 1, 24 * 3600)
-            ctx.body = {success: '成功', data: user}
-        } else {
-            await mem.set("novelUser_" + unionid, 1, 24 * 3600)
-            user = new UserModel();
-            user.nickname = '';
-            user.sex = '0';
-            user.openid = openid;
-            user.unionid = unionid;
-            user.code = code;
-            user.balance = 0;
-            user.action_time = Date.now();
-            user.save(function () {
-                ctx.body = {success: '成功', data: user}
-            })
+  let unionid = ctx.request.query.unionid
+  let openid = ctx.request.query.openid
+  let code = ctx.request.query.code
+  let user = await UserModel.findOne({
+    unionid: unionid
+  })
+  if (unionid) {
+    if (user) {
+      await mem.set("novelUser_" + unionid, 1, 24 * 3600)
+      ctx.body = {
+        success: '成功',
+        data: user
+      }
+    } else {
+      await mem.set("novelUser_" + unionid, 1, 24 * 3600)
+      user = new UserModel();
+      user.nickname = '';
+      user.sex = '0';
+      user.openid = openid;
+      user.unionid = unionid;
+      user.code = code;
+      user.balance = 0;
+      user.action_time = Date.now();
+      user.save(function () {
+        ctx.body = {
+          success: '成功',
+          data: user
         }
-    }else{
-        ctx.body = {err: "登陆失败，unionid不能为空"}
+      })
     }
+  } else {
+    ctx.body = {
+      err: "登陆失败，unionid不能为空"
+    }
+  }
 })
 
 router.get('/logout', async function (ctx, next) {
-    let unionid = ctx.request.query.unionid
-    let user = await UserModel.findOne({unionid: unionid})
-    if (user) {
-        await mem.set("novelUser_" + unionid, 0, 1)
-        ctx.body = {success: '成功', data: user}
-    } else {
-        ctx.body = {err: "退出登陆失败"}
+  let unionid = ctx.request.query.unionid
+  let user = await UserModel.findOne({
+    unionid: unionid
+  })
+  if (user) {
+    await mem.set("novelUser_" + unionid, 0, 1)
+    ctx.body = {
+      success: '成功',
+      data: user
     }
+  } else {
+    ctx.body = {
+      err: "退出登陆失败"
+    }
+  }
 })
 
 router.get('/shelf', async function (ctx, next) {
-    let unionid = ctx.request.query.unionid
-    let id = ctx.request.query.id;
-    let user = await UserModel.findOneAndUpdate({unionid: unionid}, {
-        $addToSet: {shelf: id}
-    }, {new: true})
-    if (user) {
-        ctx.body = {success: '成功', data: user}
-    } else {
-        ctx.body = {err: "添加到书架失败"}
+  let unionid = ctx.request.query.unionid
+  let id = ctx.request.query.id;
+  let user = await UserModel.findOneAndUpdate({
+    unionid: unionid
+  }, {
+    $addToSet: {
+      shelf: id
     }
+  }, {
+    new: true
+  })
+  if (user) {
+    ctx.body = {
+      success: '成功',
+      data: user
+    }
+  } else {
+    ctx.body = {
+      err: "添加到书架失败"
+    }
+  }
 })
 
 router.get('/unshelf', async function (ctx, next) {
-    let unionid = ctx.request.query.unionid
-    let id = ctx.request.query.id;
-    let user = await UserModel.findOneAndUpdate({unionid: unionid}, {
-        $pull: {shelf: id}
-    }, {new: true})
-    if (user) {
-        ctx.body = {success: '成功', data: user}
-    } else {
-        ctx.body = {err: "从书架移除失败"}
+  let unionid = ctx.request.query.unionid
+  let id = ctx.request.query.id;
+  let user = await UserModel.findOneAndUpdate({
+    unionid: unionid
+  }, {
+    $pull: {
+      shelf: id
     }
+  }, {
+    new: true
+  })
+  if (user) {
+    ctx.body = {
+      success: '成功',
+      data: user
+    }
+  } else {
+    ctx.body = {
+      err: "从书架移除失败"
+    }
+  }
 })
 
 module.exports = router
