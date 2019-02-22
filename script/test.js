5f112b43b10d2025e5d143b6aa56c7b609b4e7bf
efdssdf <efdssdf@gmail.com>
2019-02-22 10:46:03 +0800

    'fix'

--- script/test.js
+++ script/test.js
@@ -2,11 +2,10 @@ const UserModel = require('../model/User')
 
 async function a() {
     let user = await UserModel.findOne({unionid: '1'})
-    user.update({
-        $addToSet: {pay_chapter: 1540},
+    console.log(user,'------------------1')
+    await UserModel.findOneAndUpdate({unionid:'1'},{
+        $addToSet: {pay_chapter: 1740},
         $inc: {balance: -30}
     })
-    user.save()
-    console.log(user, '------------------user')
 }
 a()
