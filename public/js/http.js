8ea4ce49f29412d2640ebe53749a0eb0647c460c
DESKTOP-1C3BJVH\明星说-李鑫 <weblixin@163.com>
2019-03-01 18:19:54 +0800

    ok

--- public/js/http.js
+++ public/js/http.js
@@ -1,7 +1,8 @@
 
 function httpRequest(url, method, data, cb) {
   $.ajax({
-    url: "http://localhost:3001" + url,
+    // url: "http://localhost:3001" + url,
+    url: url,
     type: method,
     data: data,
     success: cb
