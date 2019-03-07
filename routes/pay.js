4f09d0fe727b1965a47ee0eaccd5b747d4dc548d
efdssdf <efdssdf@gmail.com>
2019-03-07 18:41:51 +0800

    'pay'

--- routes/pay.js
+++ routes/pay.js
@@ -16,7 +16,7 @@ router.get('/', async function (ctx, next) {
     let mch_id = "1527118561"
     let nonce_str = rand()
     let notify_url = "http://n.tyuss.com/pay/back"
-    let spbill_create_ip = "127.0.0.1"
+    let spbill_create_ip = "39.106.138.15"
     let price = ctx.request.query.price
     let total_fee = ctx.request.query.price * 100
     let trade_type = "APP"
@@ -27,6 +27,7 @@ router.get('/', async function (ctx, next) {
     let doc = await OrderModel.create(data)
     let out_trade_no = doc.order_number
     let str = "appid=" + appid + "&body=" + body + "&mch_id=" + mch_id + "&nonce_str=" + nonce_str + "notify_url=" + notify_url + "&out_trade_no=" + out_trade_no + "&spbill_create_ip=" + spbill_create_ip + "&total_fee=" + total_fee + "&trade_type=" + trade_type + "&key=yWaUgkQkASFQTspRJZGDelK2X7GqkXLK"
+    console.log(str,'------------------str')
     let md5 = crypto.createHash('md5');
     md5.update(str, "utf8");
     str = md5.digest('hex');
