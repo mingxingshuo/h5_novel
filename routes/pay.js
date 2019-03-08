6fe137e441782647504751801cd91266d77c8623
efdssdf <efdssdf@gmail.com>
2019-03-08 14:56:55 +0800

    'pay'

--- routes/pay.js
+++ routes/pay.js
@@ -46,7 +46,7 @@ router.get('/', async function (ctx, next) {
     let param = builder.buildObject(send_data);
     request.post({url: 'https://api.mch.weixin.qq.com/pay/unifiedorder', body: param}, function (err, res, data) {
         console.log(data, '-------------------------data');
-        parser.parseString(data.text, function (err1, result) {
+        parser.parseString(data, function (err1, result) {
             console.log(err1, result, '-------------------------result');
             ctx.body = {success: '成功'}
         });
