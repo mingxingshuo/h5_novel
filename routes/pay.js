485f80a2fa07e66136a1147386dd29d1f20b1711
efdssdf <efdssdf@gmail.com>
2019-03-08 11:42:12 +0800

    'pay'

--- routes/pay.js
+++ routes/pay.js
@@ -20,13 +20,14 @@ router.get('/', async function (ctx, next) {
     let price = ctx.request.query.price
     let total_fee = parseInt(ctx.request.query.price * 100)
     let trade_type = "APP"
+    let sign_type = "MD5"
     let data = {
         u_id: u_id,
         total_fee: price
     }
     let doc = await OrderModel.create(data)
     let out_trade_no = doc.order_number
-    let str = "appid=" + appid + "&body=" + body + "&mch_id=" + mch_id + "&nonce_str=" + nonce_str + "&notify_url=" + notify_url + "&out_trade_no=" + out_trade_no + "&spbill_create_ip=" + spbill_create_ip + "&total_fee=" + total_fee + "&trade_type=" + trade_type + "&key=yWaUgkQkASFQTspRJZGDelK2X7GqkXLK"
+    let str = "appid=" + appid + "&body=" + body + "&mch_id=" + mch_id + "&nonce_str=" + nonce_str + "&notify_url=" + notify_url + "&out_trade_no=" + out_trade_no + "&sign_type=" + sign_type + "&spbill_create_ip=" + spbill_create_ip + "&total_fee=" + total_fee + "&trade_type=" + trade_type + "&key=yWaUgkQkASFQTspRJZGDelK2X7GqkXLK"
     console.log(str, '------------------str')
     let md5 = crypto.createHash('md5');
     md5.update(str, "utf8");
@@ -43,6 +44,7 @@ router.get('/', async function (ctx, next) {
         spbill_create_ip: spbill_create_ip,
         total_fee: total_fee,
         trade_type: trade_type,
+        sign_type: sign_type,
         sign: sign
     }
     // let param = '<xml><appid>'+appid+'</appid><body>'+body+'</body><mch_id>'+mch_id+'</mch_id><nonce_str>'+nonce_str+'</nonce_str><notify_url>'+notify_url+'</notify_url><out_trade_no>'+out_trade_no+'</out_trade_no><spbill_create_ip>'+spbill_create_ip+'</spbill_create_ip><total_fee>'+total_fee+'</total_fee><trade_type>'+trade_type+'</trade_type><sign>'+sign+'</sign></xml>'
