a16952e5a42aa3223e763fe03b228ce12404ef26
efdssdf <efdssdf@gmail.com>
2019-03-11 16:38:59 +0800

    'balance'

--- routes/pay.js
+++ routes/pay.js
@@ -113,6 +113,7 @@ function req(param) {
     return new Promise((resolve, reject) => {
         request.post({url: 'https://api.mch.weixin.qq.com/pay/unifiedorder', body: param}, function (err, res, data) {
             parser.parseString(data, function (err1, result) {
+                console.log(result,'------------------result')
                 resolve({success: '成功', data: result})
             })
         })
