8ad65e2314fae40f800671d1ead28e9fbfe70a16
Administrator <lionel@buzzy.cc>
2019-03-11 18:39:20 +0800

    打印签名

--- routes/pay.js
+++ routes/pay.js
@@ -47,6 +47,7 @@ router.get('/', async function (ctx, next) {
     var h5_nonce_str = rand();
     let timeStamp = Date.parse(new Date()) / 1000;
     let str1 = "appid=" + appid + "&nonce_str=" + nonce_str + "&package=Sign=WXPay&partnerid=" + mch_id + "&prepayid=" + prepay_id + "&timeStamp=" + timeStamp + "&key=dK98AAMOJeCbqaIoCGkRJrKitN1HBfQW"
+    console.log(str1)
     let paySign = md5(str1)
     ctx.body = {
         "appid": appid,
@@ -54,7 +55,8 @@ router.get('/', async function (ctx, next) {
         "nonceStr": nonce_str,
         "prepay_id": prepay_id,
         "signType": "MD5",
-        "paySign": paySign
+        "paySign": paySign,
+        "str1" :str1
     }
 })
 
