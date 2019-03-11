b35524dc28b7ee99ccaad00f46db396c23950f27
efdssdf <efdssdf@gmail.com>
2019-03-11 17:30:45 +0800

    'balance'

--- routes/pay.js
+++ routes/pay.js
@@ -46,7 +46,7 @@ router.get('/', async function (ctx, next) {
     var prepay_id = result.xml.prepay_id[0];
     var h5_nonce_str = rand();
     let timeStamp = Date.parse(new Date()) / 1000;
-    let str1 = "appid=" + appid + "&mch_id=" + mch_id + "&nonce_str" + h5_nonce_str + "&package=Sign=WXPay&timeStamp=" + timeStamp
+    let str1 = "appid=" + appid + "&nonce_str" + h5_nonce_str + "&package=Sign=WXPay&partnerid=" + mch_id + "&prepayid=" + prepay_id + "timeStamp=" + timeStamp
     let paySign = md5(str1)
     ctx.body = {
         "appid": appid,
