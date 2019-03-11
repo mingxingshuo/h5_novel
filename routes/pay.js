12fcc933f65cc3dc0e83faaea9ec87a7ede65288
Administrator <lionel@buzzy.cc>
2019-03-11 18:56:41 +0800

    1

--- routes/pay.js
+++ routes/pay.js
@@ -46,7 +46,7 @@ router.get('/', async function (ctx, next) {
     var prepay_id = result.xml.prepay_id[0];
     var h5_nonce_str = rand();
     let timeStamp = Date.parse(new Date()) / 1000;
-    let str1 = "appid=" + appid + "&nonce_str=" + nonce_str + "&package=Sign=WXPay&partnerid=" + mch_id + "&prepayid=" + prepay_id + "&timestamp=" + timeStamp + "&key=dK98AAMOJeCbqaIoCGkRJrKitN1HBfQW"
+    let str1 = "appid=" + appid + "&noncestr=" + nonce_str + "&package=Sign=WXPay&partnerid=" + mch_id + "&prepayid=" + prepay_id + "&timestamp=" + timeStamp + "&key=dK98AAMOJeCbqaIoCGkRJrKitN1HBfQW"
     console.log(str1)
     let paySign = md5(str1)
     ctx.body = {
