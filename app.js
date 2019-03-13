b059555e940ad96b8e0e6aec6ea49a57d27dbb82
Administrator <lionel@buzzy.cc>
2019-03-13 16:52:57 +0800

    1

--- app.js
+++ app.js
@@ -22,7 +22,9 @@ onerror(app)
 app.use(bodyparser({
   enableTypes:['json', 'form', 'text']
 }))
-app.use(cors());
+
+//app.use(cors());
+
 app.use(json())
 app.use(logger())
 app.use(require('koa-static')(__dirname + '/public'))
