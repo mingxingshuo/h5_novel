81a755e286e825df338f196a5ceae7f5b578b634
efdssdf <efdssdf@gmail.com>
2019-03-20 17:14:27 +0800

    'fix'

--- routes/index.js
+++ routes/index.js
@@ -126,7 +126,7 @@ router.get('/content', async(ctx, next) => {
             return ctx.render('pages/content', {data: chapter, isfirst: isfirst, islast: islast})
         } else {
             let book = await BookModel.find({id: chapter.bid})
-            console.log(book,'----------------------------book')
+            console.log(book, book.id, book.title, '----------------------------book')
             return ctx.redirect('/recharge?bid=' + book.id + '&id=' + id + '&title=' + book.title)
         }
     } else {
