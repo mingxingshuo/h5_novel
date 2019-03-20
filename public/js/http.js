64de0de27f431f9ab7962e9f29b1bc441180c0e7
DESKTOP-1C3BJVH\明星说-李鑫 <weblixin@163.com>
2019-03-20 14:56:48 +0800

    ajax headers

--- public/js/http.js
+++ public/js/http.js
@@ -1,8 +1,14 @@
 
 function httpRequest(url, method, data, cb) {
+  let uid = JavaScriptInterface.getUid(),
+      deviceid = JavaScriptInterface.getDeviceid();
   $.ajax({
     url: url,
     type: method,
+    headers: {
+      uid: uid,
+      deviceid: deviceid
+    },
     data: data,
     success: cb
   })
