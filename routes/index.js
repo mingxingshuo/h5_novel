721f4cad0b4ab982eee0798e8a2666fd48a76622
efdssdf <efdssdf@gmail.com>
2019-04-18 19:37:00 +0800

    'fix'

--- routes/index.js
+++ routes/index.js
@@ -9,44 +9,21 @@ const PayBookModel = require("../model/PayBook")
 
 router.prefix('/')
 var price = 30
-//
-// async function httpRequest(url, id) {
-//     return new Promise((resolve, reject) => {
-//         request.get({
-//             url: url,
-//             headers: {
-//                 uid: id
-//             }
-//         }, (err, res, body) => {
-//             let data = JSON.parse(body)
-//             resolve(data)
-//         })
-//     })
-// }
-
-async function book(id) {
-    let book = await BookModel.findOne({id: id})
-    let chapters = await ChapterModel.find({bid: id}).sort({id: 1})
-    return new Promise((resolve, reject) => {
-        let first = chapters[0].id
-        let last = chapters[chapters.length - 1].id
-        resolve({book: book, first: first, last: last})
-    })
-}
 
 router.get('/content', async(ctx, next) => {
     let id = ctx.request.query.id, isfirst, islast
     let u_id = ctx.id
     let chapter = await ChapterModel.findOne({id: id})
     let book = await BookModel.findOne({id: chapter.bid})
-    let user = ctx.user
-    let result = await book(ctx.request.query.bid)
-    if (result.first == id) {
+    let chapters = await ChapterModel.find({bid: id}).sort({id: 1})
+    let first = chapters[0].id
+    let last = chapters[chapters.length - 1].id
+    if (first == id) {
         isfirst = true
     } else {
         isfirst = false
     }
-    if (result.last == id) {
+    if (last == id) {
         islast = true
     } else {
         islast = false
@@ -59,12 +36,13 @@ router.get('/content', async(ctx, next) => {
     }, {upsert: true})
     if (!chapter.isvip) {
         return ctx.render('pages/content', {data: chapter, isfirst: isfirst, islast: islast})
-    }
-    let pay_book = await PayBookModel.findOne({u_id: u_id, bid: book.id})
-    if (pay_book) {
-        return ctx.render('pages/content', {data: chapter, isfirst: isfirst, islast: islast})
-    }else{
-        return ctx.redirect('/recharge?bid=' + book.id + '&id=' + id + '&title=' + encodeURIComponent(book.title))
+    } else {
+        let pay_book = await PayBookModel.findOne({u_id: u_id, bid: book.id})
+        if (pay_book) {
+            return ctx.render('pages/content', {data: chapter, isfirst: isfirst, islast: islast})
+        } else {
+            return ctx.redirect('/recharge?bid=' + book.id + '&id=' + id + '&title=' + encodeURIComponent(book.title))
+        }
     }
 })
 
