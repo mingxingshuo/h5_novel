b9d42145a8f29d73a2dc2335120cac0c7c795c79
efdssdf <efdssdf@gmail.com>
2019-04-19 14:59:38 +0800

    'fix'

--- model/Order.js
+++ model/Order.js
@@ -8,7 +8,8 @@ autoIncrement.initialize(DB.getDB());
 var OrderSchema = new Schema({
     u_id: String,
     bid: Number,
-    total_fee:Number,
+    total_fee: Number,
+    order_number: Number,
     status: {type: Number, default: 0}, //订单是否支付，1是，0不是
     createAt: {
         type: Date,
