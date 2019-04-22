a4ca6961537a9bc6082b7a630eb47ff692e32335
efdssdf <efdssdf@gmail.com>
2019-04-22 19:02:22 +0800

    'alipay'

--- routes/alipay.js
+++ routes/alipay.js
@@ -2,9 +2,9 @@ const router = require('koa-router')()
 const request = require('request')
 const crypto = require("crypto");
 const fs = require('fs');
-const xml2js = require("xml2js");
-const builder = new xml2js.Builder();
-const parser = new xml2js.Parser();
+// const xml2js = require("xml2js");
+// const builder = new xml2js.Builder();
+// const parser = new xml2js.Parser();
 const OrderModel = require('../model/Order')
 const BookPayRuleModel = require('../model/BookPayRule');
 const AlipaySdk = require('alipay-sdk').default
@@ -31,7 +31,7 @@ router.get('/', async function (ctx, next) {
         type: 2
     })
 
-    let result = await alipaySdk.exec("http://n.tyuss.com",{
+    let result = await alipaySdk.exec("http://n.tyuss.com", {
         notifyUrl: 'http://n.tyuss.com/alipay/back',
         appAuthToken: '',
         // sdk 会自动把 bizContent 参数转换为字符串，不需要自己调用 JSON.stringify
@@ -39,9 +39,9 @@ router.get('/', async function (ctx, next) {
             subject: 'xxx',
             outTradeNo: doc._id.toString(),
             totalAmount: total_fee,
-            product_code:'QUICK_WAP_WAY'
+            product_code: 'QUICK_WAP_WAY'
         },
-    },{
+    }, {
         // 验签
         validateSign: true,
         // 打印执行日志
@@ -59,29 +59,32 @@ router.post('/back', async function (ctx, next) {
     });
     ctx.req.on('end', function () {
         buf = buf.replace('undefined', '');
-        parser.parseString(buf, function (err, data) {
-            if (err) {
-                console.log(err, ' 订单返回错误');
-            } else {
-                if (data.xml) {
-                    balan(data).then(() => {
-                        console.log('订单处理成功');
-                    })
-                } else {
-                    console.log('订单返回错误');
-                }
-            }
-        });
+        // parser.parseString(buf, function (err, data) {
+        //     if (err) {
+        //         console.log(err, ' 订单返回错误');
+        //     } else {
+        if (buf) {
+            balan(buf).then(() => {
+                console.log('订单处理成功');
+            })
+        } else {
+            console.log('订单返回错误');
+        }
+        // }
+        // });
     });
     ctx.body = "<xml><return_code><![CDATA[SUCCESS]]></return_code><return_msg><![CDATA[OK]]></return_msg></xml>";
 })
 
 async function balan(data) {
-    let out_trade_no = data.xml.out_trade_no[0]
-    await OrderModel.findOneAndUpdate({_id: out_trade_no}, {
-        status: 1,
-        updateAt: Date.now()
-    })
+    let out_trade_no = data.out_trade_no
+    let trade_status = data.trade_status
+    if(trade_status == "TRADE_SUCCESS"){
+        await OrderModel.findOneAndUpdate({_id: out_trade_no}, {
+            status: 1,
+            updateAt: Date.now()
+        })
+    }
 }
 
 module.exports = router
