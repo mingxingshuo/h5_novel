4cf50c2e885ba8f554813c53525be2084ad6c67e
efdssdf <efdssdf@gmail.com>
2019-04-23 11:40:19 +0800

    'key'

--- routes/alipay.js
+++ routes/alipay.js
@@ -8,48 +8,48 @@ const parser = new xml2js.Parser();
 const OrderModel = require('../model/Order')
 const BookPayRuleModel = require('../model/BookPayRule');
 const AlipaySdk = require('alipay-sdk').default
-const alipaySdk = new AlipaySdk({
-    appId: 'xxx',
-    privateKey: fs.readFileSync('./private-key.pem', 'ascii'),
-    alipayPublicKey: fs.readFileSync('./public-key.pem', 'ascii'),
-})
+// const alipaySdk = new AlipaySdk({
+//     appId: 'xxx',
+//     privateKey: fs.readFileSync('./private-key.pem', 'ascii'),
+//     alipayPublicKey: fs.readFileSync('./public-key.pem', 'ascii'),
+// })
 
 router.prefix('/alipay')
 
-router.get('/', async function (ctx, next) {
-    let bid = ctx.request.query.bid
-    let distribution = ctx.request.query.distribution
-    let total_fee = ctx.request.query.price
-    let u_id = ctx.id
-    let rule = BookPayRuleModel.findOne({bid: bid, price: total_fee})
-    let doc = await OrderModel.create({
-        u_id: u_id,
-        bid: bid,
-        rid: rule._id,
-        distribution: distribution,
-        total_fee: total_fee,
-        type: 2
-    })
-
-    let result = await alipaySdk.exec("http://n.tyuss.com", {
-        notifyUrl: 'http://n.tyuss.com/alipay/back',
-        appAuthToken: '',
-        // sdk 会自动把 bizContent 参数转换为字符串，不需要自己调用 JSON.stringify
-        bizContent: {
-            subject: 'xxx',
-            outTradeNo: doc._id.toString(),
-            totalAmount: total_fee,
-            product_code: 'QUICK_WAP_WAY'
-        },
-    }, {
-        // 验签
-        validateSign: true,
-        // 打印执行日志
-        log: this.logger,
-    })
-    console.log(result);
-    ctx.redirect("https://openapi.alipay.com/gateway.do")
-})
+// router.get('/', async function (ctx, next) {
+//     let bid = ctx.request.query.bid
+//     let distribution = ctx.request.query.distribution
+//     let total_fee = ctx.request.query.price
+//     let u_id = ctx.id
+//     let rule = BookPayRuleModel.findOne({bid: bid, price: total_fee})
+//     let doc = await OrderModel.create({
+//         u_id: u_id,
+//         bid: bid,
+//         rid: rule._id,
+//         distribution: distribution,
+//         total_fee: total_fee,
+//         type: 2
+//     })
+//
+//     let result = await alipaySdk.exec("http://n.tyuss.com", {
+//         notifyUrl: 'http://n.tyuss.com/alipay/back',
+//         appAuthToken: '',
+//         // sdk 会自动把 bizContent 参数转换为字符串，不需要自己调用 JSON.stringify
+//         bizContent: {
+//             subject: 'xxx',
+//             outTradeNo: doc._id.toString(),
+//             totalAmount: total_fee,
+//             product_code: 'QUICK_WAP_WAY'
+//         },
+//     }, {
+//         // 验签
+//         validateSign: true,
+//         // 打印执行日志
+//         log: this.logger,
+//     })
+//     console.log(result);
+//     ctx.redirect("https://openapi.alipay.com/gateway.do")
+// })
 
 router.post('/back', async function (ctx, next) {
     var buf = "";
