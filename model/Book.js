ffa620f02c2cd1558e9666bf4c62d2cb17968078
efdssdf <efdssdf@gmail.com>
2019-04-24 16:03:52 +0800

    'book'

--- model/Book.js
+++ model/Book.js
@@ -13,7 +13,6 @@ var BookSchema = new Schema({
     image: String,
     image_url: String,
     page_title: {type: String, default: ''},
-    chapter_title: {type: String, default: ''},
     zishu: {type: Number, default: 0},
     zhishu: {type: Number, default: 0},
     xstype: {type: Number, default: 0}, //小说状态 0连载中，1已完结
