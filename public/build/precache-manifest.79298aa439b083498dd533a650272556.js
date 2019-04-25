2aef7c219d07e3dbd46fbe3560ab6ad70a248022
DESKTOP-1C3BJVH\明星说-李鑫 <weblixin@163.com>
2019-04-25 15:17:04 +0800

    ok

---
+++ public/build/precache-manifest.79298aa439b083498dd533a650272556.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bd3599fdd1aacefdee7c",
+    "url": "/static/js/main.6a9fb8cc.chunk.js"
+  },
+  {
+    "revision": "895873d737acbd3abe98",
+    "url": "/static/js/2.5c97db60.chunk.js"
+  },
+  {
+    "revision": "bd3599fdd1aacefdee7c",
+    "url": "/static/css/main.7fb1a0fd.chunk.css"
+  },
+  {
+    "revision": "895873d737acbd3abe98",
+    "url": "/static/css/2.b0d2e6ce.chunk.css"
+  },
+  {
+    "revision": "c2185540304919272194a2461209655f",
+    "url": "/index.html"
+  }
+];
