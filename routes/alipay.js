e8a230bb86a699420d30bbd9984321ce7b7cb5cd
efdssdf <efdssdf@gmail.com>
2019-04-25 18:30:30 +0800

    'fix'

--- routes/alipay.js
+++ routes/alipay.js
@@ -37,7 +37,7 @@ router.get('/', async function (ctx, next) {
         type: 2
     })
     try {
-        let result = await alipaySdk.exec("alipay.trade.close", {
+        let result = await alipaySdk.exec("alipay.trade.wap.pay", {
             notifyUrl: 'http://p.tyuss.com/alipay/back',
             // sdk 会自动把 bizContent 参数转换为字符串，不需要自己调用 JSON.stringify
             bizContent: {
