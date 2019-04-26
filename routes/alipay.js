7d127fe9d7a574fae5956d91fd76e86b4dd3bf3d
efdssdf <efdssdf@gmail.com>
2019-04-26 13:07:39 +0800

    'pay'

--- routes/alipay.js
+++ routes/alipay.js
@@ -37,11 +37,11 @@ router.get('/', async function (ctx, next) {
             appAuthToken: '',
             // sdk 会自动把 bizContent 参数转换为字符串，不需要自己调用 JSON.stringify
             bizContent: {
-                subject: encodeURIComponent('黑牛全本小说'),
                 outTradeNo: doc._id.toString(),
-                totalAmount: total_fee,
                 productCode: 'QUICK_WAP_WAY',
-                quitUrl:'http://p.rrtvz.com/alipay/content'
+                quitUrl:'http://p.rrtvz.com/alipay/content',
+                subject: encodeURIComponent('黑牛全本小说'),
+                totalAmount: total_fee
             },
         }, {
             // 验签
