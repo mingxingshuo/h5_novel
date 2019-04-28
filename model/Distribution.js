e57f27d8bad70f28e042c3c68c15a02ec663e769
DESKTOP-1C3BJVH\明星说-李鑫 <weblixin@163.com>
2019-04-28 15:10:38 +0800

    ok

--- model/Distribution.js
+++ model/Distribution.js
@@ -4,27 +4,33 @@ var Schema = mongoose.Schema;
 var DB = require('./DB');
 
 var DistributionSchema = new Schema({
-    title:{
+  title: {
+    type: String,
+    default: ''
+  },//广告位名称
+  adzones: Object,//接入广告配置
+  /*code:{
+          platform:String,//广告平台
+          adkey:String
+  }*/
+  official: {//公众号配置
+    image: {
       type: String,
       default: ''
-    },//广告位名称
-    adzones:Object,//接入广告配置
-    /*code:{
-            platform:String,//广告平台
-            adkey:String
-    }*/
-    official:{//公众号配置
-    	image:{
-    	  type: String,
-        default: ''
-      },
-    	name : {
-        type: String,
-        default: ''
-      },
-        appid : String,
-        appsecret : String
+    },
+    name: {
+      type: String,
+      default: ''
+    },
+    appid: {
+      type: String,
+      default: ''
+    },
+    appsecret: {
+      type: String,
+      default: ''
     }
+  }
 
 });
 
