9b93a4e8ee0a25ce73cf149461cba5ab1dd5fd59
efdssdf <efdssdf@gmail.com>
2019-04-29 13:21:37 +0800

    'wxpay'

--- routes/wxpay.js
+++ routes/wxpay.js
@@ -84,6 +84,7 @@ router.get('/', async function (ctx, next) {
 })
 
 router.post('/back', async function (ctx, next) {
+    console.log(ctx.request.body,'----------------------body')
     var buf = "";
     ctx.req.setEncoding('utf8');
     ctx.req.on('data', function (chunk) {
