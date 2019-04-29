91db20f249e5056ff0640150d17eac9765a395ea
DESKTOP-1C3BJVH\明星说-李鑫 <weblixin@163.com>
2019-04-29 11:13:27 +0800

    ok

--- public/build/service-worker.js
+++ public/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5f7ecb2c9040a279c482a4c34241d5d8.js"
+  "/precache-manifest.e8ca5fcd08c609870f2a49585b177bec.js"
 );
 
 workbox.clientsClaim();
